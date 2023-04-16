0bae06378334b3521eae2c007ad3b6d00c236f5c
SajalRuhela017 <ruhela.sajal017@gmail.com>
2023-04-17 00:37:57 +0530

    Initialized DAO

---
+++ app/src/main/java/com/androiddevs/dailydigest/db/ArticleDao.kt
@@ -0,0 +1,22 @@
+package com.androiddevs.dailydigest.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.androiddevs.dailydigest.Article
+
+@Dao
+interface ArticleDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(article: Article): Long
+
+    @Query("SELECT * FROM articles")            // This function will return live data
+    fun getArticles(): LiveData<List<Article>>
+
+    @Delete
+    suspend fun deleteArticle(article: Article)
+}
