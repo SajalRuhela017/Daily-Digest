515188b5059220e73c1a58fcccdb81ef1f6dc4d7
SajalRuhela017 <ruhela.sajal017@gmail.com>
2023-04-17 00:29:49 +0530

    Retrofit Setup Successful

---
+++ app/src/main/java/com/androiddevs/dailydigest/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.dailydigest.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "a629c54bbc614f9a8735ec1bd7efbe85"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
