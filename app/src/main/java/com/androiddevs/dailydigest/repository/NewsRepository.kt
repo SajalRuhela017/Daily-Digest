bdd3c57da0674461b45af8c3fa5b61621f7abf14
SajalRuhela017 <ruhela.sajal017@gmail.com>
2023-04-27 00:28:39 +0530

    Upadted Some Files

--- app/src/main/java/com/androiddevs/dailydigest/repository/NewsRepository.kt
+++ app/src/main/java/com/androiddevs/dailydigest/repository/NewsRepository.kt
@@ -1,6 +1,10 @@
 package com.androiddevs.dailydigest.repository
 
+import com.androiddevs.dailydigest.api.RetrofitInstance
 import com.androiddevs.dailydigest.db.ArticleDatabase
 
 class NewsRepository(val db: ArticleDatabase) {
+    suspend fun getBreakingNews(countryCode: String, pageNumber: Int) {
+        RetrofitInstance.api.getBreakingNews(countryCode, pageNumber)
+    }
 }
--- app/src/main/java/com/androiddevs/dailydigest/ui/NewsViewModel.kt
+++ app/src/main/java/com/androiddevs/dailydigest/ui/NewsViewModel.kt
@@ -1,8 +1,12 @@
 package com.androiddevs.dailydigest.ui
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.androiddevs.dailydigest.models.NewsResponse
 import com.androiddevs.dailydigest.repository.NewsRepository
+import com.androiddevs.dailydigest.util.Resource
 
 class NewsViewModel(val newsRepository: NewsRepository) : ViewModel(){
 
+    val breakingNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
 }
