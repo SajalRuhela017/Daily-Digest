b232c4698bdb822aebe97a775fe07f5c72f5b275
SajalRuhela017 <ruhela.sajal017@gmail.com>
2023-05-05 19:10:29 +0530

    Added WebView Functionality

--- app/src/main/java/com/androiddevs/dailydigest/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/androiddevs/dailydigest/ui/fragments/ArticleFragment.kt
@@ -2,17 +2,26 @@ package com.androiddevs.dailydigest.ui.fragments
 
 import android.os.Bundle
 import android.view.View
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.androiddevs.dailydigest.R
 import com.androiddevs.dailydigest.ui.NewsActivity
 import com.androiddevs.dailydigest.ui.NewsViewModel
+import kotlinx.android.synthetic.main.fragment_article.*
 
 class ArticleFragment : Fragment(R.layout.fragment_article) {
 
-    private lateinit var viewModel: NewsViewModel
+    lateinit var viewModel: NewsViewModel
+    val args: ArticleFragmentArgs by navArgs()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = (activity as NewsActivity).viewModel
+        val article = args.article
+        webView.apply {
+            webViewClient = WebViewClient()
+            loadUrl(article.url)
+        }
     }
 }
--- app/src/main/java/com/androiddevs/dailydigest/ui/fragments/SavedNewsFragment.kt
+++ app/src/main/java/com/androiddevs/dailydigest/ui/fragments/SavedNewsFragment.kt
@@ -3,15 +3,38 @@ package com.androiddevs.dailydigest.ui.fragments
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.androiddevs.dailydigest.R
+import com.androiddevs.dailydigest.adapters.NewsAdapter
 import com.androiddevs.dailydigest.ui.NewsActivity
 import com.androiddevs.dailydigest.ui.NewsViewModel
+import kotlinx.android.synthetic.main.fragment_breaking_news.*
+import kotlinx.android.synthetic.main.fragment_saved_news.*
 
 class SavedNewsFragment : Fragment(R.layout.fragment_saved_news) {
 
-    private lateinit var viewModel: NewsViewModel
+    lateinit var viewModel: NewsViewModel
+    lateinit var newsAdapter: NewsAdapter
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
             super.onViewCreated(view, savedInstanceState)
             viewModel = (activity as NewsActivity).viewModel
+
+        newsAdapter.setOnItemClickListener {
+            val bundle = Bundle().apply {
+                putSerializable("article", it)
+            }
+            findNavController().navigate(
+                R.id.action_savedNewsFragment_to_articleFragment,
+                bundle
+            )
+        }
+    }
+    private fun setupRecyclerView() {
+        newsAdapter = NewsAdapter()
+        rvSavedNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
     }
 }
